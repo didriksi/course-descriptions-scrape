7a0ecff339d74fead9a138d860d9ecb28ca90c64
Didrik Sten Ingebrigtsen <didriksten@gmail.com>
2021-05-19 09:38:11 +0200

    Split dataset, and test performance

--- trainModel.py
+++ trainModel.py
@@ -2,26 +2,39 @@ import fasttext
 import re
 import pandas as pd
 
-def make_dataset(course_df):
+def make_dataset(course_df, split=0.7):
     """Makes a fasttext-friendly list of course descriptions from a dataframe.
 
     :param course_df: pd.DataFrame with columns coursecode, coursename, course_description.
+    :param split: Float 0-1 determining size of training set.
 
-    :return: List of strings.
+    :return: Two-tuple of list of strings, training set and then testing set.
     """
 
-    dataset = []
-    for (_, course) in course_df.iterrows():
-        if course["course_description"] and isinstance(course["course_description"], str):
+    train = []; test = []
+    valid_course_df = course_df.dropna()
+    valid_course_df = valid_course_df[valid_course_df["course_description"] != ""]
+    institute_splits = {institute_name: int(len(group)*split)
+                        for institute_name, group in valid_course_df.groupby("institute")}
+    for (_, course) in valid_course_df.iterrows():
+        if institute_splits[course["institute"]]:
             course_desc = re.sub('\n', ' ', course["course_description"])
-            dataset.append(
-                "__label__{institute} {course_desc}".format(**course, course_desc=course_desc))
-    return dataset
+            train.append(
+                "__label__{institute} {course_desc}".format(**course, course_desc=course_desc)
+            )
+            institute_splits[course["institute"]] -= 1
+        else:
+            course_desc = re.sub('\n', ' ', course["course_description"])
+            test.append(
+                "__label__{institute} {course_desc}".format(**course, course_desc=course_desc)
+            )
+    return train, test
 
 if __name__ == "__main__":
     course_df = pd.read_csv("courses.csv")
-    dataset = make_dataset(course_df)
-    print(course_df)
-    with open("dataset.txt", "w") as file:
-        file.write("\n".join(dataset))
-    model = fasttext.train_supervised("dataset.txt", epoch=30, lr=1.0, wordNgrams=3, verbose=2)
+    train, test = make_dataset(course_df)
+    for split_var, split_name in [(test, "test"), (train, "train")]:
+        with open(f"{split_name}.txt", "w") as file:
+            file.write("\n".join(split_var))
+    model = fasttext.train_supervised("train.txt", epoch=30, lr=1.0, wordNgrams=3, verbose=2)
+    print(model.test("test.txt"))
